2e049bf161bd326dee53de7be62b7647516a414f
dchala8 <d.chala@uniandes.edu.co>
2024-03-03 02:20:22 -0500

    FIX

--- src/inquilinos/modulos/inquilinos/infraestructura/mapeadores.py
+++ src/inquilinos/modulos/inquilinos/infraestructura/mapeadores.py
@@ -6,13 +6,12 @@ encargados de la transformación entre formatos de dominio y DTOs
 """
 
 from inquilinos.seedwork.dominio.repositorios import Mapeador
-from inquilinos.modulos.inquilinos.dominio.objetos_valor import Fecha_inicio, Fecha_fin, Monto
+from inquilinos.modulos.inquilinos.dominio.objetos_valor import nombre, telefono
 from inquilinos.modulos.inquilinos.dominio.entidades import Inquilino
 from .dto import Inquilino as InquilinoDTO
 import uuid
 
 class MapeadorInquilino(Mapeador):
-    _FORMATO_FECHA = '%Y-%m-%dT%H:%M:%SZ'
 
     def obtener_tipo(self) -> type:
         return Inquilino.__class__
@@ -21,8 +20,8 @@ class MapeadorInquilino(Mapeador):
         print("entidad_a_dto_infra")
         print(entidad)
         inquilino_dto = InquilinoDTO()
-        inquilino_dto.fecha_creacion = entidad.fecha_creacion
-        inquilino_dto.fecha_actualizacion = entidad.fecha_actualizacion
+        inquilino_dto.nombre = entidad.nombre
+        inquilino_dto.telefono = entidad.telefono
         inquilino_dto.id = entidad.id
         inquilino_dto.fecha_inicio = entidad.fecha_inicio 
         inquilino_dto.fecha_fin = entidad.fecha_fin
@@ -36,12 +35,8 @@ class MapeadorInquilino(Mapeador):
     def dto_a_entidad(self, dto: InquilinoDTO) -> Inquilino:
         print("dto_a_entidad_infra")
         print(InquilinoDTO)
-        inquilino = Inquilino(dto.id, dto.fecha_creacion, dto.fecha_actualizacion)
-        inquilino.fecha_inicio = dto.fecha_inicio
-        inquilino.fecha_fin = dto.fecha_fin
-        inquilino.id_propiedad = dto.id_propiedad
-        inquilino.id_inquilino = dto.id_inquilino
-        inquilino.id_compania = dto.id_compania
-        inquilino.monto = dto.monto
+        inquilino = Inquilino(dto.id, dto.nombre, dto.telefono)
+        inquilino.fecha_inicio = dto.nombre
+        inquilino.fecha_fin = dto.telefono
         
         return inquilino
