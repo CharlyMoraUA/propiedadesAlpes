058682f6ff690eea5ad1411df6ca06034b379178
Carlos Mora <ca.morar1@uniandes.edu.co>
2024-03-04 06:20:43 +0000

    procesador agregado

---
+++ src/procesador/modulos/procesador/infraestructura/consumidores.py
@@ -0,0 +1,51 @@
+import pulsar,_pulsar  
+from pulsar.schema import *
+import uuid
+import time
+import logging
+import traceback
+
+from procesador.modulos.procesador.infraestructura.schema.v1.eventos import EventoProcesadorCreado
+from procesador.modulos.procesador.infraestructura.schema.v1.comandos import ComandoCrearProcesador
+from procesador.seedwork.infraestructura import utils
+
+def suscribirse_a_eventos():
+    cliente = None
+    try:
+        cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
+        consumidor = cliente.subscribe('eventos-propiedad', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='propiedades-sub-eventos-procesador', schema=AvroSchema(EventoProcesadorCreado))
+
+        while True:
+            mensaje = consumidor.receive()
+            print(f'Evento recibido: {mensaje.value().data}')
+
+            consumidor.acknowledge(mensaje)     
+
+        cliente.close()
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de eventos!')
+        traceback.print_exc()
+        if cliente:
+            cliente.close()
+
+def suscribirse_a_comandos():
+    cliente = None
+    try:
+        cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
+        consumidor = cliente.subscribe('comandos-propiedad', consumer_type=_pulsar.ConsumerType.Shared, subscription_name='propiedades-sub-comandos-procesador', schema=AvroSchema(ComandoCrearProcesador))
+
+        while True:
+            mensaje = consumidor.receive()
+            print(f'====================================================================')
+            print(f'====================================================================')
+            print(f'====================================================================')
+            print(f'Comando recibido: {mensaje.value().data}')
+
+            consumidor.acknowledge(mensaje)     
+            
+        cliente.close()
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de comandos!')
+        traceback.print_exc()
+        if cliente:
+            cliente.close()
