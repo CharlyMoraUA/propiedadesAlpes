59d598f30c5e10a100ef6f0e46a0deb147dc5a22
dchala8 <d.chala@uniandes.edu.co>
2024-03-08 23:45:37 -0500

    fix

--- src/inquilinos/modulos/inquilinos/aplicacion/queries/base.py
+++ src/inquilinos/modulos/inquilinos/aplicacion/queries/base.py
@@ -1,5 +1,5 @@
 from inquilinos.seedwork.aplicacion.queries import QueryHandler
-from inquilinos.modulos.inquilinos.infraestructura.fabricas import FabricaRepositorio
+from inquilinos.modulos.inquilinos.infraestructura.fabricas import FabricaRepositorio, FabricaVista
 from inquilinos.modulos.inquilinos.dominio.fabricas import FabricaInquilinos
 
 class InquilinoQueryBaseHandler(QueryHandler):
@@ -7,6 +7,10 @@ class InquilinoQueryBaseHandler(QueryHandler):
         self._fabrica_repositorio: FabricaRepositorio = FabricaRepositorio()
         self._fabrica_inquilinos: FabricaInquilinos = FabricaInquilinos()
 
+    @property
+    def fabrica_vista(self):
+        return self._fabrica_vista
+    
     @property
     def fabrica_repositorio(self):
         return self._fabrica_repositorio
