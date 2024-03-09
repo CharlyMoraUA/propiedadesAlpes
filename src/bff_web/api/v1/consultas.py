10b6247bd0e9ee661f971a8c4b5d9faed3147702
dchala8 <d.chala@uniandes.edu.co>
2024-03-08 21:57:41 -0500

    Ajuste a GET Individual

--- src/bff_web/api/v1/consultas.py
+++ src/bff_web/api/v1/consultas.py
@@ -5,7 +5,7 @@ from .esquemas import *
 @strawberry.type
 class Query:
     contratos: typing.List[Contrato] = strawberry.field(resolver=obtener_contratos)
-    #contrato: Contrato = strawberry.field(resolver=obtener_contrato)
+    
     @strawberry.field
     def contrato(self, id: str) -> Contrato:
-        return Contrato(id=id)
+        return obtener_contrato(id)
