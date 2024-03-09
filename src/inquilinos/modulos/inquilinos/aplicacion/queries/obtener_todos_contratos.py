c9d3f3d832800c35fe33128b533f1566c79d03c9
dchala8 <d.chala@uniandes.edu.co>
2024-03-08 22:52:08 -0500

    Se agrega GET ALL a inquilinos

--- src/inquilinos/modulos/inquilinos/aplicacion/queries/obtener_todos_contratos.py
+++
@@ -1,9 +0,0 @@
-from inquilinos.seedwork.aplicacion.queries import Query, QueryHandler, ResultadoQuery
-
-class ObtenerTodoInquilinos(Query):
-    ...
-
-class ObtenerTodosInquilinosHandler(QueryHandler):
-
-    def handle() -> ResultadoQuery:
-        ...
--- src/inquilinos/modulos/inquilinos/infraestructura/consumidores.py
+++ src/inquilinos/modulos/inquilinos/infraestructura/consumidores.py
@@ -32,7 +32,7 @@ def suscribirse_a_comandos():
     cliente = None
     try:
         cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
-        consumidor = cliente.subscribe('comandos-inquilino', consumer_type=_pulsar.ConsumerType.Shared, subscription_name='inquilinos-sub-comandos', schema=AvroSchema(ComandoCrearInquilino))
+        consumidor = cliente.subscribe('comandos-inquilino', consumer_type=_pulsar.ConsumerType.Shared, subscription_name='inquilinos-sub-comandos-inquilino', schema=AvroSchema(ComandoCrearInquilino))
 
         while True:
             mensaje = consumidor.receive()
