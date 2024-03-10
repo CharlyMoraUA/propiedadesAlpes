05327029a7fd5e4a388d0d71f3e2ffa0fe6a7cd7
dchala8 <d.chala@uniandes.edu.co>
2024-03-10 16:03:31 -0500

    Se ajusta Inquilinos a mutador

--- src/inquilinos/modulos/inquilinos/infraestructura/schema/v1/comandos.py
+++ src/inquilinos/modulos/inquilinos/infraestructura/schema/v1/comandos.py
@@ -1,10 +1,28 @@
 from pulsar.schema import *
 from dataclasses import dataclass, field
 from inquilinos.seedwork.infraestructura.schema.v1.comandos import (ComandoIntegracion)
+from aeroalpes.seedwork.infraestructura.utils import time_millis
+import uuid
 
 class ComandoCrearInquilinoPayload(ComandoIntegracion):
     id_usuario = String()
     # TODO Cree los records 
 
 class ComandoCrearInquilino(ComandoIntegracion):
-    data = ComandoCrearInquilinoPayload()
+    data = ComandoCrearInquilinoPayload() 
+    
+    
+class ComandoInquilinoPayload(ComandoIntegracion):
+    nombre = String()
+    telefono = Integer()
+    id = String()
+
+class ComandoInquilino(ComandoIntegracion):
+    id = String(default=str(uuid.uuid4()))
+    time = Long()
+    specversion = String()
+    type = String()
+    ingestion = Long(default=time_millis())
+    datacontenttype = String()
+    service_name = String()
+    data = ComandoInquilinoPayload()
